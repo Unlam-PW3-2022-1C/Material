4b1ab356a6b2b7da16afcb708cd4d057ec4f81f2
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2022-04-13 20:46:11 -0300

    agregando lista partidos (hardcoded)

---
+++ Clase-3-MVC/Clase-3-MVC.Web/Controllers/PartidosController.cs
@@ -0,0 +1,66 @@
+﻿using Clase_3_MVC.Web.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clase_3_MVC.Web.Controllers
+{
+    public class PartidosController : Controller
+    {
+        // GET: PartidosController
+        public ActionResult Lista()
+        {
+            List<PartidoViewModel> partidos = new List<PartidoViewModel>()
+            {
+                new PartidoViewModel() {  Fecha = new DateTime(2022, 4, 12), Lugar = "La Bombonera" },
+                new PartidoViewModel() {  Fecha = new DateTime(2022, 4, 13), Lugar = "El Monumental" },
+            };
+            return View(partidos);
+        }
+
+        // GET: PartidosController/Nuevo
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+
+        // POST: PartidosController/Nuevo
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Nuevo(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PartidosController/Eliminar/5
+        public ActionResult Eliminar(int id)
+        {
+            return View();
+        }
+
+        // POST: PartidosController/Eliminar/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Eliminar(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Clase-3-MVC/Clase-3-MVC.Web/Models/PartidoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clase_3_MVC.Web.Models
+{
+    public class PartidoViewModel
+    {
+        public DateTime Fecha { get; set; }
+        public string Lugar { get; set; }
+    }
+}
