8d49f201906b93ade493874cac480f583052acef
ignaciodamiang <ignaciodamiang@gmail.com>
2022-04-19 19:02:16 -0300

    servicios+action deldia

---
+++ Clase-3-MVC/Clase-3-MVC.Web/Services/IPartidosService.cs
@@ -0,0 +1,17 @@
+﻿using Clase_3_MVC.Web.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Clase_3_MVC.Web.Services
+{
+    public interface IPartidosService
+    {
+        IEnumerable<PartidoViewModel> Lista();
+        void NuevoBindingManual(IFormCollection form);
+        void Nuevo(PartidoViewModel partido);
+        PartidoViewModel Encontrar(int id);
+        void Eliminar(int id);
+        IEnumerable<PartidoViewModel> EncontrarPartidos(int dia, int mes, int anio);
+    }
+}
