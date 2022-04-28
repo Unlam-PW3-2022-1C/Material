ee1d897cf0c084d4921bad7e2be8f831fbd7b280
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2022-04-27 21:13:00 -0300

    agregando formulario de alta usuario y lista

---
+++ Clase-Layouts-PartialViews-Validaciones/Clase-Layouts-PartialViews-Validaciones/Models/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace Clase_Layouts_PartialViews_Validaciones.Models
+{
+    public class Usuario
+    {
+        public string Nombre { get; set; }
+        public string AvatarURL { get; set; }
+        public string Email { get; set; }
+        public int Puntos { get; set; }
+    }
+}
