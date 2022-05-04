fa4b742c72dc3a52b978160695fdc98b39a4d68d
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2022-05-04 20:19:41 -0300

    agregando ejemplos de ViewData ViewBag y TempData

---
+++ Clase-Pasaje-De-Datos/Clase-Pasaje-De-Datos/Clase-Pasaje-De-Datos/Controllers/CalculadorDeGastosController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Clase_Pasaje_De_Datos.Controllers
+{
+    public class CalculadorDeGastosController : Controller
+    {
+        private static readonly string[] Meses = new[]
+        {
+            "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+        };
+
+        public IActionResult Index()
+        {
+            //ejemplo ViewData
+            ViewData["Meses"] = Meses;
+            //ejemplo ViewBag
+            ViewBag.MesActual = Meses[DateTime.Now.Month - 1];
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string mes)
+        {
+            int index = Array.IndexOf(Meses, mes);
+            if (index > 6)
+            {
+                TempData["Gastos"] = 50000;
+            }
+            else
+            {
+                TempData["Gastos"] = 0;
+            }
+            return Redirect($"/CalculadorDeGastos/Resultado");
+        }
+
+        public IActionResult Resultado()
+        {
+            return View();
+        }
+    }
+}
