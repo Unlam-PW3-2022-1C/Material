0c09f87ef9e7f9921291cc418747b6b63ceaf4a4
pablokuko <pablokuko@gmail.com>
2022-05-04 21:11:00 -0300

    Feature/pasaje de datos (#11)
    * agregando ejemplos de ViewData ViewBag y TempData
    
    * agregando uso de session usando getstring y setstring y tambien con SessionHelper

---
+++ Clase-Pasaje-De-Datos/Clase-Pasaje-De-Datos/Clase-Pasaje-De-Datos/Controllers/CalculadorDeGastosController.cs
@@ -0,0 +1,63 @@
+﻿using Clase_Pasaje_De_Datos.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+
+namespace Clase_Pasaje_De_Datos.Controllers
+{
+    public class CalculadorDeGastosController : Controller
+    {
+        private static readonly string[] Meses = new[]
+        {
+            "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+        };
+
+        private ISessionHelper _sessionHelper;
+
+        public CalculadorDeGastosController(ISessionHelper sessionHelper)
+        {
+            _sessionHelper = sessionHelper;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["UltimoMes"] = HttpContext.Session.GetString("UltimoMesCalculado");
+
+            //otra alternativa centralizando las keys de session
+            //ViewData["UltimoMes"] = _sessionHelper.UltimoMesCalculado;
+
+            //ejemplo ViewData
+            ViewData["Meses"] = Meses;
+            //ejemplo ViewBag
+            ViewBag.MesActual = Meses[DateTime.Now.Month - 1];
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string mes)
+        {
+            HttpContext.Session.SetString("UltimoMesCalculado", mes);
+
+            //otra alternativa centralizando las keys de session
+            //_sessionHelper.UltimoMesCalculado = mes;
+
+            int index = Array.IndexOf(Meses, mes);
+            if (index > 6)
+            {
+                TempData["Gastos"] = 50000;
+            }
+            else
+            {
+                TempData["Gastos"] = 0;
+            }
+            return Redirect($"/CalculadorDeGastos/Resultado");
+        }
+
+        public IActionResult Resultado()
+        {
+            return View();
+        }
+    }
+}
