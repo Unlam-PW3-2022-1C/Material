fa1d92940811e8127b9a483b2af340d77dfa2739
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2022-06-22 19:45:16 -0300

    agregando Rest CRUD endpoints para Animal

---
+++ Clase-Web-Api/ClasificadorAnimales/Models/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClasificadorAnimales.Models
+{
+    public class Animal
+    {
+        public int IdAnimal { get; set; }
+        public string NombreEspecie { get; set; }
+        public double PesoPromedio { get; set; }
+        public int EdadPromedio { get; set; }
+    }
+}
